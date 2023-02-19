0b4601bc99174bba5e9cec08aaafc691d4632661
Hithru <hithrudealwis@gmail.com>
2023-02-19 21:59:11 +0530

    Add admin officer dashboard officer view update color scheme

--- lib/screens/admin/admin_dashboard.dart
+++ lib/screens/admin/admin_dashboard.dart
@@ -6,6 +6,7 @@ import 'package:crop_damage_assessment_app/services/database.dart';
 import 'package:flutter/material.dart';
 import 'package:crop_damage_assessment_app/services/auth.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../officer/add_officer.dart';
 
@@ -24,178 +25,141 @@ class _AdminDashboardState extends State<AdminDashboard> {
   late DatabaseService db;
 
   bool loading = true;
+  var filter = {"user_type": "officer"};
+
+  void initOfficerClaimList() async {
+    db = DatabaseService(uid: widget.uid);
+    db.set_select_uid = widget.uid!;
+
+    final preference = await SharedPreferences.getInstance();
+    setState(() {
+      loading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initOfficerClaimList();
+    print(filter);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-          body: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            SliverAppBar(
-              title: const Text('Crop Assister - Admin'),
-                        backgroundColor:
-                            const Color.fromARGB(255, 122, 156, 122),
-                        elevation: 0.0,
-                        // automaticallyImplyLeading: false,
-                        actions:                        
-                            <Widget>[                              
-                          IconButton(
-                              icon: const Icon(Icons.power_settings_new),
-                              onPressed: () async {
-                                await _auth.signoutUser(widget.key, context);
-                              })
-                        ])];
-        },
-        body: Container(
-              padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 50.0),            
-                      
-                      child: Column(
-                        children: <Widget>[
-                          const SizedBox(height: 1.0),
-                          Image.network(
-                              'https://firebasestorage.googleapis.com/v0/b/crop-assister.appspot.com/o/Crop%20assister%20app%20%20PNG.png?alt=media&token=e9067fd2-4eac-4df4-93be-185589e15833'),
-                          const SizedBox(height: 5.0),
-                          const Text('WELCOME',
-                              style: TextStyle(
-                                  fontStyle: FontStyle.normal,
-                                  fontSize: 30,
-                                  color: Color.fromARGB(255, 4, 92, 9))),
-                          const SizedBox(height: 10.0),
-                           const Text('“CropAssister” is mobile application to submit the compensation claims together with evidence of damage (E.g. photos/videos) which allows the crop damage compensation process to be more effective and less fraudulent.',
-                              style: TextStyle(
-                                  fontStyle: FontStyle.normal,
-                                  fontSize: 13,                                  
-                                  color: Color.fromARGB(255, 4, 92, 9)),
-                                  textAlign:TextAlign.center),
-                          const SizedBox(height: 30.0),
-                          Padding(
-      padding: const EdgeInsets.only(top: 8.0),
-      child: Card(
-        elevation: 5,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-              const SizedBox(height: 10.0),
-            ListTile(             
-
-              title: Text("ALL CLAIMS",
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16.0,
-                      color: Color.fromARGB(255, 0, 0, 0)),
-                      textAlign: TextAlign.center,),                      
-              subtitle: Row(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment :CrossAxisAlignment.center,
-                children: <Widget>[
-                   
-                   const SizedBox(width: 5.0),
-                   
-                  Align(
-                    alignment: Alignment.center,                                        
+    return StreamProvider<List<Officer?>>.value(
+        value: db.officerList,
+        initialData: const [],
+        builder: (context, snapshot) {
+          return DefaultTabController(
+            length: 2,
+            child: Scaffold(
+              body: NestedScrollView(
+                  headerSliverBuilder:
+                      (BuildContext context, bool innerBoxIsScrolled) {
+                    return <Widget>[
+                      SliverAppBar(
+                          title: const Text('Crop Assister - Admin'),
+                          backgroundColor:
+                              const Color.fromARGB(255, 122, 156, 122),
+                          elevation: 0.0,
+                          // automaticallyImplyLeading: false,
+                          actions: <Widget>[
+                            IconButton(
+                                icon: const Icon(Icons.power_settings_new),
+                                onPressed: () async {
+                                  await _auth.signoutUser(widget.key, context);
+                                })
+                          ])
+                    ];
+                  },
+                  body: Container(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 1.0, horizontal: 50.0),
                     child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment :CrossAxisAlignment.center,
-                children: <Widget>[
-                   
-                   const SizedBox(height: 10.0)                                           
-                 
-                ],
-              ),            
-                  ),
-                  
-                ],
-              ), 
-                        
-                           
-            ),
-              const SizedBox(height: 20.0) 
-          ],
-        ),
-      ),
-    ), const SizedBox(height: 25.0),
-                          ElevatedButton(
-                             child: const Text('GET START'),
-                             style: ElevatedButton.styleFrom(
-                              primary: const Color.fromARGB(
-                                  255, 71, 143, 75), // background
-                              onPrimary: Colors.white, // foreground
-                              textStyle: const TextStyle(fontSize: 20),
-                              shape: new RoundedRectangleBorder(
-                                borderRadius: new BorderRadius.circular(20.0),
-                              ),
-                              padding: const EdgeInsets.symmetric(
-                                  vertical: 10.0, horizontal: 50.0),
-                            ), onPressed: () { 
-                               Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    AddOfficer()),
-                          );
-                             },),
-                             Text(
+                      children: <Widget>[
+                        const SizedBox(height: 1.0),
+                        Image.network(
+                            'https://firebasestorage.googleapis.com/v0/b/crop-assister.appspot.com/o/Crop%20assister%20app%20%20PNG.png?alt=media&token=e9067fd2-4eac-4df4-93be-185589e15833'),
+                        const SizedBox(height: 5.0),
+                        const Text('WELCOME',
                             style: TextStyle(
                                 fontStyle: FontStyle.normal,
-                                fontSize: 15,
-                                color: Color.fromARGB(255, 4, 92, 9)),
-                            'Add New Officer', //title
-                            textAlign: TextAlign.end, //aligment
+                                fontSize: 30,
+                                color: Color.fromARGB(255, 4, 92, 9))),
+                        const SizedBox(height: 10.0),
+                        const SizedBox(height: 25.0),
+                        ElevatedButton(
+                          child: const Text('Add New Officer'),
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color.fromARGB(
+                                255, 71, 143, 75), // background
+                            onPrimary: Colors.white, // foreground
+                            textStyle: const TextStyle(fontSize: 20),
+                            shape: new RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(20.0),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 50.0),
                           ),
-                              
-                          const SizedBox(height: 12.0),
-                         
-                        ],
-                      ),
-                    )                
-            
-      ),
-         drawer: Drawer(
-                  child: ListView(
-                    padding: EdgeInsets.zero,
-                    children: [
-                      const DrawerHeader(
-                        decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 105, 184, 109),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => AddOfficer()),
+                            );
+                          },
                         ),
-                        child: Text('Crop Assister'),
+                        Expanded(
+                          child: ViewOfficerList(
+                            uid: widget.uid,
+                            officer_type: 'officer',
+                          ),
+                        ),
+                        const SizedBox(height: 12.0),
+                      ],
+                    ),
+                  )),
+              drawer: Drawer(
+                child: ListView(
+                  padding: EdgeInsets.zero,
+                  children: [
+                    const DrawerHeader(
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 105, 184, 109),
                       ),
-                       ListTile(
-                        title: const Text('Home'),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    AdminDashboard(uid: widget.uid)),
-                          );
-                        },
-                      ),
-                       ListTile(
-                        title: const Text('Add Officers'),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    AddOfficer()),
-                          );
-                        },
-                      ),
-                    
-                      
-                      ListTile(
-                        title: const Text('Logout'),
-                        onTap: () async {
-                          await _auth.signoutUser(widget.key, context);
-                        },
-                      ),
-                    ],
-                  ),
+                      child: Text('Crop Assister'),
+                    ),
+                    ListTile(
+                      title: const Text('Home'),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  AdminDashboard(uid: widget.uid)),
+                        );
+                      },
+                    ),
+                    ListTile(
+                      title: const Text('Add Officers'),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => AddOfficer()),
+                        );
+                      },
+                    ),
+                    ListTile(
+                      title: const Text('Logout'),
+                      onTap: () async {
+                        await _auth.signoutUser(widget.key, context);
+                      },
+                    ),
+                  ],
                 ),
-      ),
-    );
+              ),
+            ),
+          );
+        });
   }
 }
--- lib/screens/admin/officer_profile.dart
+++ lib/screens/admin/officer_profile.dart
@@ -20,13 +20,13 @@ class OfficerProfile extends StatefulWidget {
 }
 
 class _OfficerProfileState extends State<OfficerProfile> {
-
   bool loading = true;
   final AuthService _auth = AuthService();
 
   void getUserProfileData() async {
     //use a Async-await function to get the data
-    final select_user = await DatabaseService(uid: widget.uid).getOfficerData(widget.officer_uid);
+    final select_user = await DatabaseService(uid: widget.uid)
+        .getOfficerData(widget.officer_uid);
     setState(() {
       user = select_user;
       loading = false;
@@ -35,7 +35,6 @@ class _OfficerProfileState extends State<OfficerProfile> {
 
   @override
   void initState() {
-
     super.initState();
     getUserProfileData();
   }
@@ -71,11 +70,11 @@ class _OfficerProfileState extends State<OfficerProfile> {
     return loading
         ? const Loading()
         : Scaffold(
-            backgroundColor: const Color.fromARGB(255, 242, 255, 243),
+            backgroundColor: const Color.fromARGB(255, 255, 255, 255),
             resizeToAvoidBottomInset: false,
             appBar: AppBar(
               title: const Text('View Officer Profile'),
-              backgroundColor: const Color.fromARGB(255, 115, 162, 177),
+              backgroundColor: const Color.fromARGB(255, 105, 184, 109),
               elevation: 0.0,
               actions: <Widget>[
                 TextButton.icon(
@@ -90,129 +89,76 @@ class _OfficerProfileState extends State<OfficerProfile> {
               ],
             ),
             body: SingleChildScrollView(
-              padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-
+              padding:
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
               child: Column(
-                  children: <Widget>[
-
-                    const SizedBox(height: 20.0),
-                    CircleAvatar(
-                      backgroundImage: NetworkImage(user!.profile_url),
-                      radius: 100,
+                children: <Widget>[
+                  const SizedBox(height: 20.0),
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(user!.profile_url),
+                    radius: 100,
+                  ),
+                  const SizedBox(height: 20.0),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      user!.name,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                          color: Color.fromARGB(255, 80, 79, 79)),
                     ),
-
-                    const SizedBox(height: 20.0),
-                    Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        user!.name,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18.0,
-                            color: Color.fromARGB(255, 80, 79, 79)),
-                      ),
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      user!.email,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                          color: Color.fromARGB(255, 80, 79, 79)),
                     ),
-                    Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        user!.email,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18.0,
-                            color: Color.fromARGB(255, 80, 79, 79)),
-                      ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Officer Details",
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          color: Color.fromARGB(255, 0, 0, 0)),
                     ),
-
-                    const SizedBox(height: 20.0),
-                    const Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "Officer Details",
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20.0,
-                            color: Color.fromARGB(255, 0, 0, 0)),
-                      ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Phone No - " +
+                          user!.phone_no +
+                          "\nStatus - " +
+                          user!.type +
+                          "\nAgrarian Division - " +
+                          user!.agrarian_division +
+                          "\nProvice - " +
+                          user!.province +
+                          "\nAddress - " +
+                          user!.address +
+                          "\nNIC - " +
+                          user!.nic,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                          color: Color.fromARGB(255, 80, 79, 79)),
                     ),
-
-                    const SizedBox(height: 10.0),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                                              
-                        "Phone No - " + 
-                        user!.phone_no + 
-
-                        "\nStatus - " + 
-                        user!.type + 
-                        
-                        "\nAgrarian Division - " + 
-                        user!.agrarian_division + 
-
-                        "\nProvice - " + 
-                        user!.province + 
-
-                        "\nAddress - " + 
-                        user!.address + 
-
-                        "\nNIC - " + 
-                        user!.nic
-                        ,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18.0,
-                            color: Color.fromARGB(255, 80, 79, 79)),
-                      ),
-                    ),
-
-                    const SizedBox(height: 20.0),
-                    ElevatedButton(
-                      child: const Text(
-                              'Approve',
-                              style: TextStyle(color: Colors.white),
-                      ),
-                      onPressed: () async {
-
-                          setState(() {
-                            loading = true;
-                          });
-
-                        var officer_data = {
-                              "uid": user!.uid,
-                              "phone_no": user!.phone_no,
-                              "name": user!.name,
-                              "email": user!.email,
-                              "type": "officer",
-                              "agrarian_division": user!.agrarian_division,
-                              "nic": user!.nic,
-                              "address": user!.address,
-                              "province": user!.province,
-                              "profile_url": user!.profile_url
-                        };
-
-                        DatabaseService db = DatabaseService(uid: widget.uid);
-                        bool isSuccess = await db.updateOfficerData(user!.uid, officer_data);
-                                      
-                        setState(() {
-                          loading = false;
-                        });
-
-                        if (isSuccess) {
-                          triggerSuccessAlert();
-                        } else {
-                          triggerErrorAlert();
-                        }
-
-                      }
-                    ),
-
-                  ],
-                ),
-          )
-        );
+                  ),
+                  const SizedBox(height: 20.0),
+                ],
+              ),
+            ));
   }
 }
